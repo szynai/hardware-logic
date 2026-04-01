0098bf785f2186aa8ccf22469db3b0d37acea718
Julia Maneclang <juliamaneclang@gmail.com>
2026-04-01 15:42:45 +0800

    hardware-feature

--- Task#4/src/hardware/Hardware.java
+++ Task#4/src/hardware/Hardware.java
@@ -9,5 +9,30 @@ package hardware;
  * @author redelyn
  */
 public class Hardware {
-    
+    protected int id;
+    protected String brand;
+    protected int spec;
+
+    public Hardware(int id, String brand, int spec) {
+        this.id = id;
+        this.brand = brand;
+        this.spec = spec;
+    }
+
+    public int getSpec() {
+        return spec;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getType() {
+        return "Unknown";
+    }
+
+    public String interpretSpec() {
+        return spec + "";
+    }
 }
+
