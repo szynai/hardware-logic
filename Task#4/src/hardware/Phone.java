e104305127f2a980a3e829451b6685fe0bb0eaef
shaznay <shaznayanneojeda@gmail.com>
2026-04-01 15:50:13 +0800

    phone-feature

--- Task#4/src/hardware/Phone.java
+++ Task#4/src/hardware/Phone.java
@@ -8,6 +8,20 @@ package hardware;
  *
  * @author redelyn
  */
-public class Phone {
-    
+public class Phone extends Hardware {
+
+    public Phone(int id, String brand, int spec) {
+        super(id, brand, spec);
+    }
+
+    @Override
+    public String getType() {
+        return "Phone";
+    }
+
+    @Override
+    public String interpretSpec() {
+        return spec + " Megapixels";
+    }
 }
+
